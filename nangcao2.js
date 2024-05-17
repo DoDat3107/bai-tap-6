15f43fb07facf483eb4810e896f1724cf1ebe4de
DoDat3107 <NaoTomori3107@gmail.com>
2024-05-18 04:02:00 +0700

    lan1

---
+++ nangcao2.js
@@ -0,0 +1,8 @@
+let a = prompt("nhập giá trị")
+    b = prompt("nhập giá trị")
+    a = parseFloat(a);
+    b = parseFloat(b);
+    h = a + b
+    console.log(h);
+        document.write(h)
+        alert(h)
